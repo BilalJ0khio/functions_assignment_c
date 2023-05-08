a2f684fa5dd7fb6b28d1cd8f503d79447ba26454
BilalJ0khio <jokhiobilal.11@gmail.com>
2023-05-08 14:03:18 +0500

    Initial Commit

---
+++ assignment function/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  assignment function
+//
+//  Created by Bilal Ahmed on 08/05/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
